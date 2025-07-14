b1ad05e6363076f93dd1b10614607148632bd772
longle325 <lebaolong74@gmail.com>
2025-07-14 10:19:43 +0700

    update index file for knowledge graph

--- src/core/index.ts
+++ src/core/index.ts
@@ -7,3 +7,4 @@ export * from './env.js';
 // Storage modules with namespace disambiguation
 export * as Storage from './storage/index.js';
 export * as VectorStorage from './vector_storage/index.js';
+export * as KnowledgeGraph from './knowledge_graph/index.js';
